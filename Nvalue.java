48cf11affbc56ebe211a9ce5dc9e34619f9c8e6f
leoinfant <leoinfant@users.noreply.github.com>
2017-06-23 12:47:03 +0530

    Add files via upload

---
+++ Nvalue.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Nvalue {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int m=0;
+		for(int i=0;i<=N;i++)
+		{
+			m=m+i;;
+			}
+			System.out.println(m);
+		}
+		
+		
+	}
+
+
