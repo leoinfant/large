944dc710ef89ea3b5112cc5c5aede10873a342c7
leoinfant <leoinfant@users.noreply.github.com>
2017-06-24 12:43:21 +0530

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Compare {
+
+	public static void main(String[] args) {
+		Scanner n=new Scanner(System.in);
+		String s=n.next();
+		String s1=n.next();
+		System.out.println(s.compareTo(s1));
+
+	}
+
+}
