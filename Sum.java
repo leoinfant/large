bb28244e6b61b0ab705e61f71a82cb68d33e93c7
leoinfant <leoinfant@users.noreply.github.com>
2017-06-24 12:04:58 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args){
+		Scanner n=new Scanner(System.in);
+		int n1=n.nextInt();
+		int n0=0;
+		for(int i=1;i<=n1;i++){
+			n0=n0+n1;
+			}
+		System.out.println(n0);
+		
+	}
+
+}
