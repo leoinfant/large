aca41c49c6b747ed409c7dacd6a2fb79ca7263b5
leoinfant <leoinfant@users.noreply.github.com>
2017-06-24 12:19:35 +0530

    Add files via upload

---
+++ Givenno.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Givenno {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		System.out.println(s1.length());
+		
+	}
+
+}
